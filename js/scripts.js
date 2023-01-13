f2eccb1fca9580f688aef195840e773d74f90415
ibukunoluwanap <promisenap2001@gmail.com>
2023-01-13 13:10:35 +0200

    updated portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
-/*!
-* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
-
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
